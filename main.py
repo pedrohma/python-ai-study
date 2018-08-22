8967550a83a3f0529d8fa8de9c93fa051f41965b
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-08-22 14:24:25 -0700

    scaling - normalization

--- main.py
+++ main.py
@@ -16,5 +16,22 @@ print("\nBinarized data:\n", data_binarized)
 # Mean Removal - eliminating the mean from feature vector so every feature is centered on zero.
 
 data_scaled = preprocessing.scale(input_data)
-print("Mean =", data_scaled.mean(axis=0))
+print("\nMean =", data_scaled.mean(axis=0))
 print("Std deviation =", data_scaled.std(axis = 0))
+
+# Scaling - the values of every feature can vary between many random values, it avoid any feature to be synthetically large or small
+
+data_scaler_minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
+data_scaled_minmax = data_scaler_minmax.fit_transform(input_data)
+print ("\nMin max scaled data:\n", data_scaled_minmax)
+
+# Normalization - modify the feature vectors
+# L1 - Least Absolute Deviations - This kind of normalization modifies the values so that the sum of the absolute values is always up to 1 in each row
+
+data_normalized_l1 = preprocessing.normalize(input_data, norm = 'l1')
+print("\nL1 normalized data:\n", data_normalized_l1)
+
+# L2 - Least Squares - This kind of normalization modifies the values so that the sum of the squares is always up to 1 in each row.
+
+data_normalized_l2 = preprocessing.normalize(input_data, norm = 'l2')
+print("\nL2 normalized data:\n", data_normalized_l2)
