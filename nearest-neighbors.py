cd3891685e9d64d6ed07a4495f805691b786d809
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-09-17 12:41:27 -0700

    neighbors 2

--- nearest-neighbors.py
+++ nearest-neighbors.py
@@ -17,7 +17,25 @@ k = 3
 
 test_data = [3.3, 2.9]
 
+# plt.figure()
+# plt.title('Input data')
+# plt.scatter(A[:,0], A[:,1], marker = 'o', s = 100, color = 'red')
+# plt.show()
+
+# building the K Nearest Neighbor
+
+knn_model = NearestNeighbors(n_neighbors = k, algorithm = 'auto').fit(A)
+distances, indices = knn_model.kneighbors([test_data])
+
+print("\nK Nearest Neighbors:")
+for rank, index in enumerate(indices[0][:k], start = 1):
+   print(str(rank) + " is", A[index])
+
 plt.figure()
-plt.title('Input data')
-plt.scatter(A[:,0], A[:,1], marker = 'o', s = 100, color = 'red')
+plt.title('Nearest neighbors')
+plt.scatter(A[:, 0], A[:, 1], marker = 'o', s = 100, color = 'k')
+plt.scatter(A[indices][0][:][:, 0], A[indices][0][:][:, 1],
+   marker = 'o', s = 250, color = 'k', facecolors = 'none')
+plt.scatter(test_data[0], test_data[1],
+   marker = 'x', s = 100, color = 'k')
 plt.show()
